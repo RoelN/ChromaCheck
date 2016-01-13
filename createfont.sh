8a4d4ef4a6f1a051f1f2e024319fd5a119d203a6
Roel Nieskens <roel@kabisa.nl>
2016-01-13 22:00:30 +0100

    Use woff, leaner ttx source, basic color check implemented

--- createfont.sh
+++ createfont.sh
@@ -2,6 +2,7 @@
 
 ttx ./src/colorfontsupport.ttx
 mv ./src/colorfontsupport.ttf .
-# sfnt2woff-zopfli colorfontsupport.ttf
-openssl base64 -A -in colorfontsupport.ttf -out colorfontsupport.base64
-rm *.ttf
+sfnt2woff-zopfli colorfontsupport.ttf
+openssl base64 -A -in colorfontsupport.woff -out colorfontsupport.base64
+rm colorfontsupport.ttf
+rm colorfontsupport.woff
