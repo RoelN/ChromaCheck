c1476ced09c26dd6f7851436ad5fb341cf551a08
Roel Nieskens <roel@kabisa.nl>
2016-01-16 20:29:33 +0100

    Removed more unused tables, used minimal <name> table

--- createfont.sh
+++ createfont.sh
@@ -4,5 +4,5 @@ ttx ./src/colorfontsupport.ttx
 mv ./src/colorfontsupport.ttf .
 sfnt2woff-zopfli colorfontsupport.ttf
 openssl base64 -A -in colorfontsupport.woff -out colorfontsupport.base64
-# rm colorfontsupport.ttf
+rm colorfontsupport.ttf
 rm colorfontsupport.woff
