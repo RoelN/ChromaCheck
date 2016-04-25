369ca448a6d96864f340bec52d5c10aaf2cdae4f
Roel Nieskens <roel@pixelambacht.nl>
2016-04-25 12:55:19 +0200

    Split CDBT to separate font

--- chromacheck.js
+++ chromacheck.js
@@ -1,5 +1,5 @@
 /**
- * ChromaCheck 1.0
+ * ChromaCheck 1.1
  * @author Roel Nieskens, https://pixelambacht.nl
  * MIT license
  */
@@ -25,8 +25,8 @@
   // Font loaded, now check for which color glyphs we see
   function colorGlyphTest() {
     var res = {};
-    res.svg  = context.getImageData(10, 30, 1, 1).data[0] === 50;  // SVG-in-OpenType
-    res.cbdt = context.getImageData(10, 50, 1, 1).data[0] === 100; // CBDT/CBLC
+    res.cbdt = context.getImageData(10, 30, 1, 1).data[0] === 100; // CBDT/CBLC
+    res.svg  = context.getImageData(10, 50, 1, 1).data[0] === 50;  // SVG-in-OpenType
     res.sbix = context.getImageData(10, 70, 1, 1).data[0] === 150; // SBIX
     res.colr = context.getImageData(10, 90, 1, 1).data[0] === 200; // COLR/CPAL
 
@@ -47,12 +47,20 @@
     var canvas = document.createElement('canvas'),
         context = canvas.getContext('2d'),
         img = new Image()
-        font = 'd09GRgABAAAAAAT4ABAAAAAABhwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABDQkRUAAACyAAAAGAAAABlKWauy0NCTEMAAAMoAAAAMAAAAFDwXcDbQ09MUgAAA1gAAAAXAAAAGAACACZDUEFMAAADcAAAABIAAAASyQAAEE9TLzIAAAHUAAAAOgAAAGAX8SdOU1ZHIAAAA4QAAADRAAABSPZDdzRjbWFwAAACHAAAACcAAAAsAA3pN2dseWYAAAJUAAAAJgAAADgzUzQ+aGVhZAAAAWwAAAAvAAAANgjn4wNoaGVhAAABnAAAABUAAAAkCAEEAmhtdHgAAAIQAAAACwAAABAEAAAAbG9jYQAAAkQAAAAOAAAAEAAnADZtYXhwAAABtAAAAB0AAAAgAhMAHm5hbWUAAAJ8AAAAPwAAAEwEbg0VcG9zdAAAArwAAAAMAAAAIAADAABzYml4AAAEWAAAAJ4AAAEYJUa0bXgBY2BkYABhdne2t/H8Nl8ZpFkYQODSAiYBEH1ZcvY3EM3CABbnYGACUQDcMgcVAHgBY2BkYGBhAAI4CRRBBYwAAZEAEAAAAHgBY2BkYGBgZ2AF0wxQkouBgYmBQQLEBAAClwA0AAAAeAFjYGFhYJzAwMrAwDST6QwDA0M/hGZ8zWDMyMmAChgFkDgKQMBw4CXDS6AJIAAk0YECAwMAAE4IeAAAeAFjYUAFAABQAAUAeAEFwbEBgAAIA7AAjO6u/v8ZV9QEZRQe68NaxW3CSd5W8ANVlwYrAHgBYwABXjiUAQAB8wBeAAB4AWNgZAACFiBkYGZgEFQUVGRh+MMAwkgyLBAZQagUiA8Aa/ME9wAAeAEthoUBQAAABE+zhBHMIBYwgu7u7fXnASImEoJsIGDBz+LLPiUHKQ0OLREhMQUL3r0TPS3hyykx9QXyCwmMAHgBY2BmwAsAAH0ABHgBY2BiYAgIACEGhojOAD93Xi4pLiCb19PDJQhIB4AwIzOQ3HI/4R2QYg7wCXFNYWB4sSRODcgV8HRxDKlgTF4gLcG6OEEEKF/AwHhn4vvJDEDg6ernss4poQkArdUV+3gBY2BiAAFGILYAYgkomyEACEEAnc3AysAaEKDACKKBPA6gakEgzQKVTQQAbG4C6XgBY2BgYARiPiAWAbKYQHyQCAACRAApAAAAAAEAAQABAAAADgAAAADI/wAAeAFtD4VuQjGwc983XG6uvc6tEF2MxeaL4X3Najh8Pe5w7sI6sNmVC2yljYzttk16idetgWquWNLeSRSXhJBzGZ/VTkn8/Hi9eMB4bBO68FKqKmjXu5LEqFwOT5zXarXL2vWlLyp+RUS8XYG9kqe60e5/XqF4fHzk3SyObxYIOitRmUaIbrG3s7tVQbmYcqW8L1qJpUzK5I7F+YU4GdZ0q0KqHEFeGyNx7/qK2oDQHvdGIOjqBhIdOW4SJKgnOu5Q/yHwsd1cDZ/v/BZrATOsUKkAAAB4AWNgBEIGMOZh1GHwANIqqJiRBwUDQUFeukJngJ87L5cUF5DL6+nhEgSU0wFhDmagiN/iOncgJVniGlESnJ9WUp5YlMrgmJKflKrgmZuYnhqUmphSWXgy1QaoiC3AJ8R1GgMI5EX4TAQZ7+niGFJx681BRpCrmhT+r8/zcAAyaQE+5DHxMCScZmC0mFYlAhLwdPVzWeeU0AQABiwmcQAA';
+        font = 'd09GRgABAAAAAARMAA4AAAAABUwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABDT0xSAAACrAAAABcAAAAYAAIAJkNQQUwAAALEAAAAEgAAABLJAAAQT1MvMgAAAawAAAA6AAAAYBfxJ05TVkcgAAAC2AAAANEAAAFI9kJ3MmNtYXAAAAH0AAAALgAAADTpG+k+Z2x5ZgAAAjQAAAAmAAAAODNTND5oZWFkAAABRAAAAC8AAAA2CRHx7GhoZWEAAAF0AAAAFQAAACQIAQQCaG10eAAAAegAAAALAAAADgQAAABsb2NhAAACJAAAAA4AAAAOADYAGm1heHAAAAGMAAAAHQAAACACEgAebmFtZQAAAlwAAABDAAAATgSgDQdwb3N0AAACoAAAAAwAAAAgAAMAAHNiaXgAAAOsAAAAngAAARQlRrNFeAFjYGRgAOF3mWnn4/ltvjJIszCAwKUFTAIg+rLzqvsgmoUBLM7BwASiABocCIIAeAFjYGRgYGEAAjgJFEEFjAABkQAQAAAAeAFjYGRgYGBjYAXTDFCSi4GBiYFBAsQEAAJ8ADMAAAB4AWNgYWFgnMDAysDANJPpDAMDQz+EZnzNYMzIyYAKGAWQOApAwHDgJcNLoAkgACTRgQIDAwAATgh4AAB4AWNhQAYAAEYABQB4ARXFsQGAIAwAsABtJ3de8P+v+oSiWYJhGbiEGyUMs2fH+9L+99zDAA6gHAgnAAAAAAAAAA0ADQANAA0AHAAAeAFjYGQAAhYgZGBmYBBUFFRkYfjDAMJIMiwQGUGoFIgPAGvzBPcAAHgBLcZVAYQAAECxd4aEQCIQASlABNzd26P3tQFvVD68vjIvDPj/fd8lZyOmwqImwCckY0LHOR1oqfHvx4SUBwCGCbAAeAFjYGbACwAAfQAEeAFjYGBgBGI+IBYBsphAfJAIAAJEACkAAAAAAQABAAEAAAAOAAAAAMj/AAB4AW0PhU5DMfCREEG/4XK49opbtyixEcOJzfsaKm++9/dUcDh3yQLMRzmTzXrMsmVv0lV1ajSMO/2BclYg3yeEjm25trJS4P3d9d4ZVivzEOFqMJbg6+1AYD4cFheMTSaT/cnhvutLdkBEzFdgKrmYamVf/yvk5+fnLGbx+2aOoNoCpS6L/AjjzrRVwrDfsIOu6xuBg1ZDdzb57h7f+lbjq4rGMIeu0lrgyuEBeUDw424IOB0cQS3KbyZBjZKI7od+QWDfdjP5+Xz4rfIGMJ9QpgAAAHgBY2AEQgYw5mHUYfAA0gqomJEDjoGgIC9doTPAz52XS4oLyOX19HAJAsrpgDAHM1DEb3GdO5CSLHGNKAnOTyspTyxKZXBMyU9KVfDMTUxPDUpNTKksPJlqA1TEFuAT4jqNAQTyInwmgoz3dHEMqbj15iAjyEVNCv/X53k4AJm0AB/ymHgYEk4zMFpMqxIBCXi6+rmsc0poAgDaiyZIAAA=',
+        fontCBDT = 'd09GRgABAAAAAAKoAAoAAAAAAuwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABDQkRUAAACGAAAAGAAAABlKWauy0NCTEMAAAJ4AAAALQAAAFDwVcDTT1MvMgAAAVgAAAA6AAAAYBf0J01jbWFwAAABnAAAACcAAAAsAAzpNmhlYWQAAAD0AAAALwAAADYJEe9yaGhlYQAAASQAAAAVAAAAJAgBBAJobXR4AAABlAAAAAYAAAAGBAAAAG1heHAAAAE8AAAAGwAAACACDgAebmFtZQAAAcQAAABHAAAAWAUnDdFwb3N0AAACDAAAAAwAAAAgAAMAAHgBY2BkYADhsHtPFsXz23xlkGZhAIFLC5gEQPRl5xWpIJqFASzOwcAEogAYQAg0AHgBY2BkYGBhAAI4CRRBBYwAAZEAEAAAAHgBY2BkYGBgYmAF0wxQkgskxCABYgIAAhAALwB4AWNgYWFgnMDAysDANJPpDAMDQz+EZnzNYMzIyYAKGAWQOApAwHDgJfNLZhYwH0iiAwUGBgAAjgh6AAAEAAAAAAAAAHgBY2BgYGRgBmIGBh4GFgYFIM0ChED+S+b//yGk2D+wPAMAXIMHKAB4AR3ItQHCABRAwcNhCGQOKqxHB8DdffsEfvUEWTU5mXxFRhPhsuEDO18rR20nczMLW08NXVt3Fyez8JWFQ/yOnkkKbAELAQB4AWNgZsALAAB9AAR4AWNgYmAICAAhBoaIzgA/d14uKS4gm9fTwyUISAeAMCMzkNxyP+EdkGIO8AlxTWFgeLEkTg3IFfB0cQypYExeIC3BujhBBChfwMB4Z+L7yQxA4Onq57LOKaEJAK3VFft4AWNgYgABRiC2AGIJKJshAAhhAMEGyzIGBCgASbA6DiApCKRZoLKJAGrOAtkAAAA=',
         svg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="100" style="background:#fff">'+
-              '<style type="text/css">@font-face{font-family:"chromacheck";src:url(data:application/x-font-woff;base64,'+font+') format("woff");}</style>'+
-              '<text x="0" y="20" fill="#000" font-family="chromacheck" font-size="20">&#xe904;<tspan x="0" dy="20">&#xe903;</tspan><tspan x="0" dy="20">&#xe902;</tspan><tspan x="0" dy="20">&#xe901;</tspan><tspan x="0" dy="20">&#xe900;</tspan></text>'+
+              '<style type="text/css">'+
+              '@font-face{font-family:"chromacheck";src:url(data:application/x-font-woff;base64,'+font+') format("woff");}'+
+              '@font-face{font-family:"chromacheck-cbdt";src:url(data:application/x-font-woff;base64,'+fontCBDT+') format("woff");}'+
+              '</style>'+
+              '<text x="0" y="20" fill="#000" font-family="chromacheck" font-size="20">&#xe904;'+ // Control glyph
+              '<tspan font-family="chromacheck-cbdt" x="0" dy="20">&#xe903;</tspan>'+ // CBDT/CBLC
+              '<tspan x="0" dy="20">&#xe902;</tspan>'+ // SVG
+              '<tspan x="0" dy="20">&#xe901;</tspan>'+ // SBIX
+              '<tspan x="0" dy="20">&#xe900;</tspan>'+ // COLR
+              '</text>'+
               '</svg>';
-
     canvas.width  = 20;
     canvas.height = 100;
 
